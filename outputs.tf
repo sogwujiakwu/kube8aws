7d51fe4c78dccd1f92f4bce6606429598c46f679
sogwujiakwu <108236185+sogwujiakwu@users.noreply.github.com>
2023-01-17 19:13:22 -0500

    Updated outputs.tf with more outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "bastion_host_public_ip" {
   value = aws_instance.bastion.public_ip
 }
+
+output "masters_private_ip" {
+  value = aws_instance.masters.*.private_ip
+}
+
+output "workers_private_ip" {
+  value = aws_instance.workers.*.private_ip
+}
+
+output "master_lb" {
+  value = aws_lb.k8_masters_lb.id
+}
