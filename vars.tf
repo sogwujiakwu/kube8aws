1dbbcfed09aaf1fba509029845044c1228032011
sogwujiakwu <108236185+sogwujiakwu@users.noreply.github.com>
2023-01-20 13:07:31 -0500

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -8,12 +8,12 @@ variable "ami_id" {
     type = string
     default = "ami-0d73480446600f555"
 }
-
+/*
 variable "availability_zones" {
   type    = list(string)
   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
-
+*/
 variable "vpc_cidr" {
     type = string
     default = "10.0.0.0/16"
