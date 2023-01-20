f7ceeeac51d3dc2cf173eefd796b51b542e32d7e
sogwujiakwu <108236185+sogwujiakwu@users.noreply.github.com>
2023-01-20 17:02:32 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -23,3 +23,25 @@ module "vpc" {
     Environment = "dev"
   }
 }
+/*
+resource "aws_subnet" "public_subnet" {
+  count = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id = "${aws_vpc.myVpc.id}"
+  cidr_block = "10.20.${10+count.index}.0/24"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  map_public_ip_on_launch = true
+  tags {
+    Name = "PublicSubnet"
+  }
+}
+resource "aws_subnet" "private_subnet" {
+  count = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id = "${aws_vpc.myVpc.id}"
+  cidr_block = "10.20.${20+count.index}.0/24"
+  availability_zone= "${data.aws_availability_zones.available.names[count.index]}"
+  map_public_ip_on_launch = false
+  tags {
+    Name = "PrivateSubnet"
+  }
+}
+*/
