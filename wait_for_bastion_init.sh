3696dc19f8e4acbeb145447136525ad776b63364
sogwujiakwu <108236185+sogwujiakwu@users.noreply.github.com>
2023-01-25 11:20:46 -0500

    Update wait_for_bastion_init.sh

--- wait_for_bastion_init.sh
+++ wait_for_bastion_init.sh
@@ -1,5 +1,5 @@
-	 #!/bin/bash   
-	 while ! systemctl status ansible;
-      do echo "Ansbile is not running. Sleep for 5 seconds"
-	       sleep 5
-	    done
+#!/bin/bash   
+while ! systemctl status ansible;
+  do echo "Ansbile is not running. Sleep for 5 seconds"
+     sleep 5
+  done
