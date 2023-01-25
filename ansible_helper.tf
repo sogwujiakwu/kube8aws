5a2b8f59dbfd26ff3d3d57125c770d8aa649acc3
sogwujiakwu <108236185+sogwujiakwu@users.noreply.github.com>
2023-01-25 10:55:49 -0500

    Update ansible_helper.tf

--- ansible_helper.tf
+++ ansible_helper.tf
@@ -36,9 +36,10 @@ resource "null_resource" "wait_for_bastion_init" {
     inline = [
       <<-EOF 
       #!/bin/bash
-      while ! systemctl status ansible; do
-		echo "Ansbile is not running. Sleep for 5 seconds"
-		sleep 5
+      while ! systemctl status ansible;
+	  do
+	    echo "Ansbile is not running. Sleep for 5 seconds"
+	    sleep 5
 	  done
       EOF
     ] 
