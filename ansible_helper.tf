03aec96e154939eb6c6f7ee0174fd81970ecd12d
sogwujiakwu <108236185+sogwujiakwu@users.noreply.github.com>
2023-01-25 10:51:09 -0500

    Update ansible_helper.tf

--- ansible_helper.tf
+++ ansible_helper.tf
@@ -34,11 +34,13 @@ resource "null_resource" "wait_for_bastion_init" {
 
   provisioner "remote-exec" {
     inline = [
+      <<-EOF 
       #!/bin/bash
-      "while ! systemctl status ansible; do
+      while ! systemctl status ansible; do
 		echo "Ansbile is not running. Sleep for 5 seconds"
 		sleep 5
-	  done"
+	  done
+      EOF
     ] 
   }
 }
